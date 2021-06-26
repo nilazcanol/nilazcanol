d9f18053fc37492603418fb5f52bdb5242458e37
cristopheSaraosV <cristopher.saraos@gmail.com>
2021-06-25 20:43:50 -0500

    add validation in post category

--- routes/category.routes.js
+++ routes/category.routes.js
@@ -1,12 +1,17 @@
 const { Router } = require('express');
 const {	categoryGet, categoryPost,categoryPut, categoryDelete } = require('../controllers/category.controller');
+const { check } = require('express-validator')
 
 
 
 const routerCategory = Router();
 
 routerCategory.get(`/`, categoryGet);
-routerCategory.post(`/`, categoryPost);
+routerCategory.post(`/`,[
+    check('name','The name is required').not().isEmpty(),
+    check('name','The name must be a string').isString(),
+    validateFields
+], categoryPost);
 routerCategory.put(`/:id`, categoryPut);
 routerCategory.delete(`/:id`, categoryDelete);
 
