8388c435006086e1660cfe13d804940fdbdda627
cristopher <crisotopher.saraos@gmail.com>
2021-08-26 11:58:56 -0500

    add lazy loading store

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path:'store', loadChildren: ()=> import('./store/store.module').then( m => m.StoreModule )},
+    { path: '**', redirectTo:'store'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
