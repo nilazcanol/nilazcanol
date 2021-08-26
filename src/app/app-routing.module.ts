ab05247bef891aecc363bcf666d97e0564351e28
cristopher <crisotopher.saraos@gmail.com>
2021-08-26 13:52:49 -0500

    Add menu menu and pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-    { path:'store', loadChildren: ()=> import('./store/store.module').then( m => m.StoreModule )},
+    { path:'store', loadChildren: () => import('./store/store.module').then( m => m.StoreModule )},
     { path: '**', redirectTo:'store'}
 ];
 
--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -1,17 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {MenuModule} from 'primeng/menu';
 import { StoreRoutingModule } from './store-routing.module';
-import { MenuStoreComponent } from './conponents/menu-store/menu-store.component';
+import { MenuStoreComponent } from './components/menu-store/menu-store.component';
+import { CardProductComponent } from './components/card-product/card-product.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CategoryComponent } from './pages/category/category.component';
+import { ProductComponent } from './pages/product/product.component';
 
 
 @NgModule({
   declarations: [
-    MenuStoreComponent
+    MenuStoreComponent,
+    CardProductComponent,
+    HomeComponent,
+    CategoryComponent,
+    ProductComponent
   ],
   imports: [
     CommonModule,
-    StoreRoutingModule
+    StoreRoutingModule,
+    MenuModule
   ]
 })
 export class StoreModule { }
