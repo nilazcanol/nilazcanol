0a3719397b4ab98e48dbcbad35ba61f3dc059989
cristopher <crisotopher.saraos@gmail.com>
2021-08-26 11:15:55 -0500

    add components pages in category

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductComponent } from './pages/product/product.component';
+import { CategoryComponent } from './pages/category/category.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    ProductComponent,
+    CategoryComponent,
+  ],
   imports: [
     CommonModule
   ]
