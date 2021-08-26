b51776c9477e91ef623c4290f3029f0e42c93a17
cristopher <crisotopher.saraos@gmail.com>
2021-08-26 12:10:39 -0500

    Add template menu store

---
+++ src/app/store/conponents/menu-store/menu-store.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-store',
+  templateUrl: './menu-store.component.html',
+  styles: [
+  ]
+})
+export class MenuStoreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { StoreRoutingModule } from './store-routing.module';
+import { MenuStoreComponent } from './conponents/menu-store/menu-store.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MenuStoreComponent
+  ],
   imports: [
     CommonModule,
     StoreRoutingModule
