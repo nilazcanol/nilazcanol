83966e7e3d59a2de7064a7fef90fa9c0f3f0b2cf
cristopher <crisotopher.saraos@gmail.com>
2021-08-26 16:19:09 -0500

    Upgrade in home store

--- src/app/store/pages/home/home.component.ts
+++ src/app/store/pages/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core'
-import { Product } from '../../interfaces/product.interface'
 
 @Component({
   selector: 'app-home',
@@ -9,26 +8,6 @@ import { Product } from '../../interfaces/product.interface'
 export class HomeComponent implements OnInit {
   constructor() {}
 
-  mostSelledProducts: Product[] = [
-    {
-      nameProduct: 'Product 1',
-      descriptionProduct: 'Product ',
-      priceProduct:1999,
-      stockProduct:'13'
-    },
-    {
-      nameProduct: 'Product 2',
-      descriptionProduct: 'Product ',
-      priceProduct:5999,
-      stockProduct:'13'
-    },
-    {
-      nameProduct: 'Product 3',
-      descriptionProduct: 'Product ',
-      priceProduct:8999,
-      stockProduct:'13'
-    },
-  ]
 
   
   ngOnInit(): void {}
--- src/app/store/pages/product/product.component.ts
+++ src/app/store/pages/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../../interfaces/product.interface';
 
 @Component({
   selector: 'app-product',
@@ -9,7 +10,27 @@ import { Component, OnInit } from '@angular/core';
 export class ProductComponent implements OnInit {
 
   constructor() { }
-
+  
+  mostSelledProducts: Product[] = [
+    {
+      nameProduct: 'Product 1',
+      descriptionProduct: 'Product ',
+      priceProduct:1999,
+      stockProduct:'13'
+    },
+    {
+      nameProduct: 'Product 2',
+      descriptionProduct: 'Product ',
+      priceProduct:5999,
+      stockProduct:'13'
+    },
+    {
+      nameProduct: 'Product 3',
+      descriptionProduct: 'Product ',
+      priceProduct:8999,
+      stockProduct:'13'
+    },
+  ]
   ngOnInit(): void {
   }
 
