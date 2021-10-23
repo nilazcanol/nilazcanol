fd245a240eeaa586b818e35f344d315749e15173
cristopher <crisotopher.saraos@gmail.com>
2021-10-23 12:00:34 -0500

    Revert "delete import category"
    This reverts commit a96e46209c3ba8cdf240cb5763cf7f2f7cd1e2ba.

--- src/app/store/services/products.service.ts
+++ src/app/store/services/products.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { category } from '../interfaces/category.interface';
 import { Product } from '../interfaces/product.interface';
 import { resApiProduct } from '../interfaces/resApiProduct.interface';
 import { resApiProductResponse } from '../interfaces/resApiProductResponse.interface';
