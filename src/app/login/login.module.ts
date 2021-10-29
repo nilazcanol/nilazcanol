01056484c39a689c5859d3e5268154604a0b4a19
cristopher <crisotopher.saraos@gmail.com>
2021-10-29 13:04:46 -0500

    no message

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,21 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { LoginRoutingModule } from './login-routing.module';
-import { LoginComponent } from './pages/login/login.component';
-import { TemplateLoginComponent } from './components/template-login/template-login.component';
-import { RegisterComponent } from './pages/register/register.component';
 
 
 @NgModule({
-  declarations: [
-    LoginComponent,
-    TemplateLoginComponent,
-    RegisterComponent
-  ],
+  declarations: [],
   imports: [
-    CommonModule,
-    LoginRoutingModule
+    CommonModule
   ]
 })
 export class LoginModule { }
