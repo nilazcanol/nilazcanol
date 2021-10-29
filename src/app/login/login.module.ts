1d88a158c299ed54d7fd7d97ccaf385221ff706e
cristopher <crisotopher.saraos@gmail.com>
2021-10-29 13:18:25 -0500

    Revert "no message"
    This reverts commit 01056484c39a689c5859d3e5268154604a0b4a19.

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { LoginRoutingModule } from './login-routing.module';
+import { LoginComponent } from './pages/login/login.component';
+import { TemplateLoginComponent } from './components/template-login/template-login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    TemplateLoginComponent,
+    RegisterComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    LoginRoutingModule
   ]
 })
 export class LoginModule { }
