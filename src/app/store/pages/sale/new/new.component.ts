d118796b6389687eb23b0791e8807f6e62e7f0b2
cristopher <crisotopher.saraos@gmail.com>
2021-12-16 18:48:39 -0300

    Show amount with name product

--- src/app/store/pages/sale/new/new.component.ts
+++ src/app/store/pages/sale/new/new.component.ts
@@ -1,56 +1,78 @@
-import { sale } from 'src/app/store/interfaces/sales/sale.interface';
+import { SweetAlertIcon } from 'sweetalert2';
+import Swal from 'sweetalert2';
+
 import { saleProductSelected } from './../../../interfaces/sales/saleProductSelected.interface';
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Product } from 'src/app/store/interfaces/product/product.interface';
 import { ProductsService } from 'src/app/store/services/products.service';
 
-
 @Component({
-  selector: 'app-new',
-  templateUrl: './new.component.html',
-  styles: [
-    `
-     .btn-dark:hover, .btn-dark:active, .btn-dark:visited {
-      background-color: #FFC008;
-      border:#FFC008;
-      color:#212529;
-      -webkit-transition: background-color 0.5s ease-out;
-      -moz-transition: background-color 0.5s ease-out;
-      -o-transition: background-color 0.5s ease-out;
-      transition: background-color 0.5s ease-out;
-}
-
-    `
-  ]
+	selector: 'app-new',
+	templateUrl: './new.component.html',
+	styles: [
+		`
+			.btn-dark:hover,
+			.btn-dark:active,
+			.btn-dark:visited {
+				background-color: #ffc008;
+				border: #ffc008;
+				color: #212529;
+				-webkit-transition: background-color 0.5s ease-out;
+				-moz-transition: background-color 0.5s ease-out;
+				-o-transition: background-color 0.5s ease-out;
+				transition: background-color 0.5s ease-out;
+			}
+		`,
+	],
 })
 export class NewComponent implements OnInit {
+	constructor(private productService: ProductsService) {}
 
-  constructor(
-      private productService:ProductsService
-  ) { }
+	listProduct: Product[] = [];
 
-    listProduct:Product[] = []
+	shoppingCart: saleProductSelected[] = [];
 
+	page: number = 0;
 
-  shoppingCart: saleProductSelected[]= [];
+	ngOnInit(): void {
+		this.productService.getAllProducts().subscribe((res) => {
+			this.listProduct = res.products;
+		});
+	}
+	addShoppingCart(saleProductSelected: saleProductSelected) {
+		this.shoppingCart.push({
+			product: saleProductSelected.product,
+			amount: saleProductSelected.amount,
+		});
+	}
 
+	addMoreProducts() {
+		this.productService.getAllProducts(this.page + 1).subscribe((res) => {
+			
+            if (res.products.length == 0) {
+				this.showToast(
+					'No more products',
+					'',
+					'info'
+				);
+			}
+			res.products.forEach((product) => {
+				this.page += 1;
+				this.listProduct.push(product);
+			});
+		});
+	}
 
-  ngOnInit(): void {
-
-
-    this.productService.getAllProducts().subscribe( res => {
-        this.listProduct = res.products;
-    });
-
-
-
-
-  }
-  addShoppingCart(saleProductSelected:saleProductSelected){
-    this.shoppingCart.push({product:saleProductSelected.product, amount:saleProductSelected.amount});
-  }
-
-
-
+  
+	showToast(
+		title: string,
+		detai: string,
+		icon: SweetAlertIcon,
+		timeOut: number = 2000
+	) {
+		Swal.fire(title, detai, icon);
+		setInterval(() => {
+			Swal.close();
+		}, timeOut);
+	}
 }
