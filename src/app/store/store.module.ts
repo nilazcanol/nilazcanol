d6c6796af4211955fefdce681bb8563b9756fc89
cristopher <crisotopher.saraos@gmail.com>
2021-12-28 16:47:52 -0300

    import components module

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -1,13 +1,10 @@
-import { MenusModule } from './components/menus/menus.module';
 import { CommonModule } from '@angular/common';
 // * Angular
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 // * PrimeNG
 import { PrimengModule } from '../primeNg/primeng.module';
-import { MenuSaleComponent } from './components/menus/menu-sale/menu-sale.component';
-// * Menu and navBar components
-import { MenuStoreComponent } from './components/menus/menu-store/menu-store.component';
+import { ComponentsModule } from './components/components.module';
 // * Category components
 import { CategoryComponent } from './pages/category/category.component';
 // * Home component
@@ -24,13 +21,9 @@ import { HomeUsersComponent } from './pages/user/home-users/home-users.component
 import { ListUserComponent } from './pages/user/list-user/list-user.component';
 // * Category pipe
 import { CategoryPipe } from './pipes/category.pipe';
-
-
-import { CategoriesModule } from './components/categories/categories.module';
-import { ProductsModule } from './components/products/products.module';
-import { SalesModule } from './components/sales/sales.module';
-import { UsersModule } from './components/users/users.module';
 import { StoreRoutingModule } from './store-routing.module';
+
+
 
 
 
@@ -53,11 +46,7 @@ import { StoreRoutingModule } from './store-routing.module';
     ReactiveFormsModule,
     StoreRoutingModule,
     PrimengModule,
-    CategoriesModule,
-    ProductsModule,
-    SalesModule,
-    UsersModule,
-    MenusModule
+    ComponentsModule
   ]
 })
 export class StoreModule { }
