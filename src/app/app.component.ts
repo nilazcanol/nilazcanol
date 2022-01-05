5785ed81806ea16bf69075675953f4d7a42891f4
cristopher <crisotopher.saraos@gmail.com>
2022-01-05 13:36:07 -0300

    refactor change theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'storeApp';
+
+
+  themeLink = document.querySelector('#theme');
+ 
+
+  ngOnInit(): void {
+    const theme = localStorage.getItem('theme') || './assets/Css/Themes/darkBlue.css';
+    this.themeLink?.setAttribute('href',theme!);
+  }
+
 }
