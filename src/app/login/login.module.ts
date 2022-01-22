6528d9090dc2eba3d66d9c2909c95ddb09c656e4
cristopher <crisotopher.saraos@gmail.com>
2022-01-22 17:49:53 -0300

    format login

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { LoginRoutingModule } from './login-routing.module';
-import { LoginComponent } from './pages/login/login.component';
-import { TemplateLoginComponent } from './components/template-login/template-login.component';
+import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { PrimengModule } from '../primeNg/primeng.module';
+import { TemplateLoginComponent } from './components/template-login/template-login.component';
+import { LoginRoutingModule } from './login-routing.module';
+import { LoginComponent } from './pages/login/login.component';
+
 
 
 @NgModule({
