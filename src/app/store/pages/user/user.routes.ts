0bf66134e45d47c37b4d6f45ada1f67ecf6c8459
cristopher <crisotopher.saraos@gmail.com>
2022-01-22 19:13:47 -0300

    formar page user

--- src/app/store/pages/user/user.routes.ts
+++ src/app/store/pages/user/user.routes.ts
@@ -1,25 +1,23 @@
+// Angular
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+// Components
 import { IsAdminGuard } from './../../../login/guards/is-admin.guard';
 import { ListUserComponent } from './list-user/list-user.component';
 import { HomeUsersComponent } from './home-users/home-users.component';
 
-// Angular
-import { RouterModule } from '@angular/router'
-import { NgModule } from '@angular/core'
-import { Routes } from '@angular/router'
-// Components
-
 export const routes: Routes = [
-    {
-        path: 'users',
-        component: HomeUsersComponent,
-        children: [{ path: 'listUser', component: ListUserComponent }],
-        canLoad: [IsAdminGuard],
-        canActivate: [IsAdminGuard],
-    },
-]
+	{
+		path: 'users',
+		component: HomeUsersComponent,
+		children: [{ path: 'listUser', component: ListUserComponent }],
+		canLoad: [IsAdminGuard],
+		canActivate: [IsAdminGuard],
+	},
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+	imports: [RouterModule.forChild(routes)],
+	exports: [RouterModule],
 })
 export class usersRoutingModule {}
