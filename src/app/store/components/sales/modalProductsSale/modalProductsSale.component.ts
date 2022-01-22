f15c1948069e040415f69df63d40a21345c497f6
cristopher <crisotopher.saraos@gmail.com>
2022-01-22 20:50:05 -0300

    fix detail sales

--- src/app/store/components/sales/modalProductsSale/modalProductsSale.component.ts
+++ src/app/store/components/sales/modalProductsSale/modalProductsSale.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import {  Product } from './../../../interfaces/sales/saleResponseGet.inteface';
 
 @Component({
-  selector: 'app-modalProductsSale',
+  selector: 'app-modal-products-sale',
   templateUrl: './modalProductsSale.component.html',
   styleUrls: ['./modalProductsSale.component.css']
 })
